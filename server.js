61f4388720965183dd5668ffd5c13b1df7157e6b
Patricia Robinson <45735996+patrisms@users.noreply.github.com>
2021-10-29 04:30:17 -0400

    make it better

--- server.js
+++ server.js
@@ -22,9 +22,9 @@ app.prepare()
 			return handle(req, res)
 		})
 
-		server.listen(process.env.PORT || 3000, err => {
-			if (err) throw err;
-			console.log("> Ready on http://localhost:3000")
+		server.listen(3000, (err) => {
+			if (err) throw err
+			console.log('> Ready on http://localhost:3000')
 		})
 	})
 	.catch((ex) => {
