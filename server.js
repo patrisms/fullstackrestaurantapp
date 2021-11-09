c457dcafa60ecf1273c68b040ead0472fb12abcf
Patricia Robinson <45735996+patrisms@users.noreply.github.com>
2021-11-09 09:26:14 -0500

    setup for heroku deployment

--- server.js
+++ server.js
@@ -5,7 +5,9 @@ const next = require('next')
 
 const dev = process.env.NODE_ENV !== 'production'
 const app = next({ dev })
-const handle = app.getRequestHandler()
+const handle = app.getRequestHandler();
+
+const PORT = process.env.PORT || 3000;
 
 app.prepare()
 	.then(() => {
@@ -22,7 +24,7 @@ app.prepare()
 			return handle(req, res)
 		})
 
-		server.listen(3000, (err) => {
+		server.listen(PORT, (err) => {
 			if (err) throw err
 			console.log('> Ready on http://localhost:3000')
 		})
